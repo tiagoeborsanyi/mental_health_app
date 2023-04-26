c92cc68496b228397641fa46da8d79db357ffdff
Tiago Emerick <tiagoeborsanyi@gmail.com>
2023-04-26 14:38:47 -0300

    created select emoticons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/util/emoticon_face.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,6 +18,7 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.all(25.0),
           child: Column(
             children: [
+              // header
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -52,6 +54,67 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ],
+              ),
+              const SizedBox(height: 20),
+              // Search
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade600,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      'Buscar',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              //Como voce se sente?
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Como você se sente?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 18),
+              // 4 diferents faces
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  EmoticonFace(emoticonFace: '🤔', feel: 'Triste'),
+                  EmoticonFace(emoticonFace: '😁', feel: 'Agradável'),
+                  EmoticonFace(emoticonFace: '😉', feel: 'Bem'),
+                  EmoticonFace(emoticonFace: '😄', feel: 'Excelente'),
+                ],
+              ),
+
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                ),
               ),
             ],
           ),
--- lib/util/emoticon_face.dart
+++ lib/util/emoticon_face.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 
 class EmoticonFace extends StatelessWidget {
-  const EmoticonFace({Key? key}) : super(key: key);
+  final String emoticonFace, feel;
+
+  const EmoticonFace({
+    Key? key,
+    required this.emoticonFace,
+    required this.feel,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.blue.shade500,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Icon(
-        Icons.notifications,
-        color: Colors.white,
-      ),
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.blue.shade500,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Center(
+            child: Text(
+              emoticonFace,
+              style: const TextStyle(
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          feel,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
